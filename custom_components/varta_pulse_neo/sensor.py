4340a5ee41fac6da5233e961b7eab444fc64d1f2
Tobias Voll <volltobias@gmail.com>
2026-04-15 09:45:57 +0200

    Fix hassfest encoding and update metadata

--- custom_components/varta_pulse_neo/sensor.py
+++ custom_components/varta_pulse_neo/sensor.py
@@ -1,4 +1,4 @@
-﻿from __future__ import annotations
+from __future__ import annotations
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
